829da02f8bdf9da23824892c2e8a496d5dcc1329
unknown <shubhamsangale20032003@gmail.com>
2024-03-29 10:34:17 +0530

    Change in navbaritem.js

--- app/Components/navbaritem.js
+++ app/Components/navbaritem.js
@@ -1,17 +1,24 @@
 "use client"
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
-import React from "react";
+import React, { Suspense } from "react";
 
-const Navbaritem=({title,param})=>{
-    const searchParam=useSearchParams();
+const Navbaritem = ({ title, param }) => {
+    return (
+        <Suspense fallback={<div>Loading...</div>}> {/* Wrap with Suspense */}
+            <NavbarItemContent title={title} param={param} />
+        </Suspense>
+    );
+}
+
+const NavbarItemContent = ({ title, param }) => {
+    const searchParam = useSearchParams();
     const genre = searchParam.get("genre");
-  return (
-    <div className={`hover:text-amber-500 font-semibold ${genre && genre===param && "underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg" }`}>
-        <Link href={`/?genre=${param}`}>{title}</Link>
-    </div>
-     
-  )
+    return (
+        <div className={`hover:text-amber-500 font-semibold ${genre && genre === param && "underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg"}`}>
+            <Link href={`/?genre=${param}`}>{title}</Link>
+        </div>
+    );
 }
 
 export default Navbaritem;
